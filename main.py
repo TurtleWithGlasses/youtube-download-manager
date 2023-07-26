73bc0ccb4c08ccf45388d1ab8a99f185fa9b0b3a
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-07-26 18:23:36 +0300

    Downloader class implement

--- main.py
+++ main.py
@@ -20,65 +20,96 @@ class Application:
         link_label = Label(self.root, text="Paste below any YouTube video link", font=("SnowPersons", 30))
         link_label.grid(pady=(0,20))
 
-        self.link_entry_var = StringVar()
-        self.link_entry = Entry(self.root, width=70, textvariable=self.link_entry_var, font=("Agency Fb",25), fg="blue")
-        self.link_entry.grid(pady=(0,15), ipady=2)
+        self.YouTubeEntryVar = StringVar()
+        self.YouTubeEntry = Entry(self.root, width=70, textvariable=self.YouTubeEntryVar, font=("Agency Fb",25), fg="blue")
+        self.YouTubeEntry.grid(pady=(0,15), ipady=2)
 
-        self.link_entry_error = Label(self.root, text="", font=("Concert One", 20))
-        self.link_entry_error.grid(pady=(0, 8))
+        self.YouTubeEntryError = Label(self.root, text="", font=("Concert One", 20))
+        self.YouTubeEntryError.grid(pady=(0, 8))
 
-        self.file_save_label = Label(self.root, text="Choose Directory", font=("Concert One", 30))
-        self.file_save_label.grid()
+        self.YouTubeFileSaveLabel = Label(self.root, text="Choose Directory", font=("Concert One", 30))
+        self.YouTubeFileSaveLabel.grid()
 
-        self.file_save_button = Button(self.root, text="Directory", font=("Bell MY", 15), command=self.open_directory)
-        self.file_save_button.grid(pady=(10, 3))
+        self.youtubeFileDirectoryButton = Button(self.root, text="Directory", font=("Bell MY", 15), command=self.openDirectory)
+        self.youtubeFileDirectoryButton.grid(pady=(10, 3))
 
-        self.file_location_label = Label(self.root, text="", font=("Freestyle Script", 25))
-        self.file_location_label.grid()
+        self.fileLocationLabel = Label(self.root, text="", font=("Freestyle Script", 25))
+        self.fileLocationLabel.grid()
 
-        self.choose_download_type = Label(self.root, text="Choose download type", font=("Variety",30))
-        self.choose_download_type.grid()
+        self.youtubeChooseLabel = Label(self.root, text="Choose download type", font=("Variety", 30))
+        self.youtubeChooseLabel.grid()
 
-        self.download_choice = [("Audio MP3", 1), ("Video MP4", 2)]
-        self.choice_var = StringVar()
-        self.choice_var.set(1)
+        self.downloadChoices = [("Audio MP3", 1), ("Video MP4", 2)]
+        self.ChoiceVar = StringVar()
+        self.ChoiceVar.set(1)
 
-        for text, mode in self.download_choice:
-            self.youtube_choice = Radiobutton(self.root, text=text, font=("Northwest old", 25), variable=self.choice_var, value=mode)
-            self.youtube_choice.grid()
+        for text, mode in self.downloadChoices:
+            self.youtubeChoices = Radiobutton(self.root, text=text, font=("Northwest old", 25), variable=self.ChoiceVar, value=mode)
+            self.youtubeChoices.grid()
 
-        self.download_button = Button(self.root, text="Download", width=10, font=("Bell MT", 15), command=check_youtube_link())
-        self.download_button.grid(pady=(30,5))
-
-
-    def check_youtube_link(self):
-
-        self.match_link = re.match("^https://www.youtube.com/.",self.link_entry_var.get())
-        if not self.match_link:
-            self.link_entry_error.config(text="Invalid YouTube Link", fg="red")
-        elif not self.open_directory():
-            self.file_location_label.config(text="Please choose a correct directory", fg="red")
-        elif self.match_link and self.file_location_label:
-            self.download_window()
+        self.downloadButton = Button(self.root, text="Download", width=10, font=("Bell MT", 15), command=self.checkYoutubeLink)
+        self.downloadButton.grid(pady=(30,5))
 
 
-    def download_window(self):
-        self.download_file_window = Toplevel(self.root)
+    def checkYoutubeLink(self):
+
+        self.matchYoutubeLink = re.match("^https://www.youtube.com/.", self.YouTubeEntryVar.get())
+        if not self.matchYoutubeLink:
+            self.YouTubeEntryError.config(text="Invalid YouTube Link", fg="red")
+        elif not self.openDirectory:
+            self.fileLocationLabel.config(text="Please choose a correct directory", fg="red")
+        elif self.matchYoutubeLink and self.openDirectory:
+            self.downloadWindow()
+
+
+    def downloadWindow(self):
+        self.newWindow = Toplevel(self.root)
         self.root.withdraw()
+        self.newWindow.state("zoomed")
+        self.newWindow.grid_rowconfigure(0, weight=0)
+        self.newWindow.grid_columnconfigure(0, weight=1)
 
-        self.app = SecondApp(self.download_file_window, self.link_entry_var.get(), self.file_name.get(), self.choice_var.get())
+        self.app = DownloadApp(self.newWindow, self.YouTubeEntryVar.get(), self.FolderName, self.ChoiceVar.get())
 
 
+    def openDirectory(self):
+        self.FolderName = filedialog.askdirectory()
 
-
-    def open_directory(self):
-        self.file_name = filedialog.askdirectory()
-
-        if len(self.file_name) > 0:
-            self.file_location_label.config(text=self.file_name, fg="green")
+        if len(self.FolderName) > 0:
+            self.fileLocationLabel.config(text=self.FolderName, fg="green")
             return True
         else:
-            self.file_location_label.config(text="Please choose a directory", fg="red")
+            self.fileLocationLabel.config(text="Please choose a directory", fg="red")
+
+
+class DownloadApp:
+
+    def __init__(self, downloadWindow, youtubeLink, FolderName, Choices):
+        self.downloadWindow = downloadWindow
+        self.youtubeLink = youtubeLink
+        self.FolderName = FolderName
+        self.Choices = Choices
+
+        self.yt = YouTube(self.youtubeLink)
+
+        if Choices == "1":
+            self.video_type = self.yt.streams.filter(only_audio=True).first()
+            self.MaxFileSize = self.video_type.filesize
+
+        if Choices == "2":
+            self.video_type = self.yt.streams.first()
+            self.MaxFileSize = self.video_type.filesize
+
+        self.loading_label = Label(self.downloadWindow, text="Downloading...", font=("Arial", 40))
+        self.loading_label.grid(pady=(100, 0))
+
+        self.loading_percentage = Label(self.downloadWindow, text="0", fg="green", font=("Arial", 40))
+        self.loading_percentage.grid(pady=(50, 0))
+
+        self.progressbar = ttk.Progressbar(self.downloadWindow, length=500, orient="horizontal", mode="undetermined")
+        self.progressbar.grid(pady=(50, 0))
+        self.progressbar.start()
+
 
 
 if __name__ == "__main__":
