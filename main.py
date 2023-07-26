ae630b14d2049d76c4c1bc951017546f01de607f
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-07-26 16:41:41 +0300

    Implement directory & save buttons, link entries Create radio button for file type Create link check function & download window structure

--- main.py
+++ main.py
@@ -30,8 +30,55 @@ class Application:
         self.file_save_label = Label(self.root, text="Choose Directory", font=("Concert One", 30))
         self.file_save_label.grid()
 
-        self.file_save_button = Button(self.root, text="Save", font=("Bell MY", 15), command=self.open_directory)
+        self.file_save_button = Button(self.root, text="Directory", font=("Bell MY", 15), command=self.open_directory)
         self.file_save_button.grid(pady=(10, 3))
+
+        self.file_location_label = Label(self.root, text="", font=("Freestyle Script", 25))
+        self.file_location_label.grid()
+
+        self.choose_download_type = Label(self.root, text="Choose download type", font=("Variety",30))
+        self.choose_download_type.grid()
+
+        self.download_choice = [("Audio MP3", 1), ("Video MP4", 2)]
+        self.choice_var = StringVar()
+        self.choice_var.set(1)
+
+        for text, mode in self.download_choice:
+            self.youtube_choice = Radiobutton(self.root, text=text, font=("Northwest old", 25), variable=self.choice_var, value=mode)
+            self.youtube_choice.grid()
+
+        self.download_button = Button(self.root, text="Download", width=10, font=("Bell MT", 15), command=check_youtube_link())
+        self.download_button.grid(pady=(30,5))
+
+
+    def check_youtube_link(self):
+
+        self.match_link = re.match("^https://www.youtube.com/.",self.link_entry_var.get())
+        if not self.match_link:
+            self.link_entry_error.config(text="Invalid YouTube Link", fg="red")
+        elif not self.open_directory():
+            self.file_location_label.config(text="Please choose a correct directory", fg="red")
+        elif self.match_link and self.file_location_label:
+            self.download_window()
+
+
+    def download_window(self):
+        self.download_file_window = Toplevel(self.root)
+        self.root.withdraw()
+
+        self.app = SecondApp(self.download_file_window, self.link_entry_var.get(), self.file_name.get(), self.choice_var.get())
+
+
+
+
+    def open_directory(self):
+        self.file_name = filedialog.askdirectory()
+
+        if len(self.file_name) > 0:
+            self.file_location_label.config(text=self.file_name, fg="green")
+            return True
+        else:
+            self.file_location_label.config(text="Please choose a directory", fg="red")
 
 
 if __name__ == "__main__":
